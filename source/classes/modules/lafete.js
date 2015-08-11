58e984942ef2a373f15d017493ceb00491dd0401
Wasabideveloper <tobias.blaser@hsr.ch>
2015-08-11 14:54:55 +0200

    Add EventListController

--- source/classes/modules/lafete.js
+++ source/classes/modules/lafete.js
@@ -1,4 +1,9 @@
-define(['frameworks/angular'], function (Angular) {
+define(['frameworks/angular', 'app/controllers/eventListController'],
+	function (Angular, EventListController) {
+		'use strict';
 	var Lafete = Angular.module('lafete', []);
+	Lafete.controller('EventListController', EventListController);
+	EventListController.$inject = ['$scope'];
+
 	return Lafete;
 });
