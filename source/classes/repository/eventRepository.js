ee7268b49f18fd744afde26c50991ed0d269b1c5
Wasabi Developer <wasabideveloper@users.noreply.github.com>
2015-11-03 08:46:37 +0100

    Update Eventrepository to user server API for add

--- source/classes/repository/eventRepository.js
+++ source/classes/repository/eventRepository.js
@@ -8,8 +8,6 @@ define(['app/model/event'], function(Event) {
 			add: '/api/events'
 		}
 
-		this.events = [];
-
 		/**
 		 * Get all events
 		 *
@@ -26,6 +24,7 @@ define(['app/model/event'], function(Event) {
 				})
 				.error(errorCallback);
 		};
+		
 		/**
 		 * Find event by identifier
 		 *
@@ -39,81 +38,18 @@ define(['app/model/event'], function(Event) {
 				})
 				.error(errorCallback);
 		};
+		
 		/**
-		 * Add event if not already in list
+		 * Add event
 		 * @param Event event
-		 * @return boolean if added successfull
 		 */
-		this.add = function(event) {
-			if(this.get(event.id)) {
-				return false;
-			} else {
-				this.events.push(event);
-				return true;
-			}
+		this.add = function(event, successCallback, errorCallback) {
+			$http.post(this.urls.add, event)
+				.success(function(eventDTO) {
+					successCallback(Event.createFromDTO(eventDTO));
+				})
+				.error(errorCallback);
 		};
-
-		// initialization
-		this.events.push(
-			new Event(
-				'Lunch',
-				null,
-				'everyone',
-				'nothing',
-				{
-					name: 'Luncheteria',
-					street: 'Bahnhofstrasse',
-					zipCode: 8640,
-					city: 'Rapperswil'
-				},
-				{
-					begin: new Date('2015-10-10T12:00:00.000Z'),
-					end: new Date('2015-10-10T13:00:00.000Z')
-				},
-				null,
-				'76ba7b42-0534-4d1f-9c0c-5b07488b0c2c'
-			)
-		);
-		this.events.push(
-			new Event(
-				'Dinner',
-				null,
-				'everyone',
-				'nothing',
-				{
-					name: 'Dinneria',
-					street: 'Bahnhofstrasse',
-					zipCode: 8000,
-					city: 'Zürich'
-				},
-				{
-					begin: new Date('2015-04-05T18:00:00.000Z'),
-					end: new Date('2015-04-05T20:00:00.000Z')
-				},
-				null,
-				'e27726fa-5334-41d1-8db0-f3d9510fcf89'
-			)
-		);
-		this.events.push(
-			new Event(
-				'Dinner',
-				null,
-				'everyone',
-				'nothing',
-				{
-					name: 'Dinneria',
-					street: 'Wedelweg',
-					zipCode: 8640,
-					city: 'Rapperswil'
-				},
-				{
-					begin: new Date('2015-12-08T17:00:00.000Z'),
-					end: new Date('2015-12-08T19:00:00.000Z')
-				},
-				null,
-				'39f0881d-ce23-44e9-81c3-1c8157380894'
-			)
-		);
 	};
 
 	return EventRepository;
